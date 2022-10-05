782ba7eb50c9458b59f964f85be8114d362b5f9c
yjyj1023 <lyj19961023@gmail.com>
2022-10-05 20:38:50 +0900

    Calculator 클래스 메소드: plus, minus, multiple, divide

---
+++ Calculator/src/Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator {
+    public void plus(double num1, double num2){
+        System.out.println(num1+num2);
+    }
+
+    public void minus(double num1, double num2){
+        System.out.println(num1-num2);
+    }
+
+    public void multiple(double num1, double num2){
+        System.out.println(num1*num2);
+    }
+
+    public void divide(double num1, double num2){
+        System.out.println(num1/num2);
+    }
+}
