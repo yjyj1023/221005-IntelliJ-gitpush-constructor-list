f10c33713fc5b08e5c09ec67ca3bc7e4cb3ebe17
yjyj1023 <lyj19961023@gmail.com>
2022-10-05 20:39:57 +0900

    Main 클래스

---
+++ Calculator/src/CalculatorMain.java
@@ -0,0 +1,10 @@
+public class CalculatorMain {
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+
+        cal.plus(10,15);
+        cal.minus(10,15);
+        cal.multiple(10,15);
+        cal.divide(10,15);
+    }
+}
